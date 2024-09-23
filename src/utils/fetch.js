28847a3f8c2c643399adc39dad9df6232714b997
Jack <jacklee.jabra@elections.ca>
2024-09-23 12:49:39 -0400

    Added Origin Host Header to apiconfig.js

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,37 +1,37 @@
-import {backend_ip} from "../../apiconfig.json"
+import { backend_ip, hostOriginHeader } from "../../apiconfig.json"
 
-export async function getFile(format, url){
+export async function getFile(format, url) {
 
-    const response = await fetch(`${backend_ip}/${format}?url=${url}`,{
+    const response = await fetch(`${backend_ip}/${format}?url=${url}`, {
 
-        method:'GET',
-	headers:{
-		
-		'Origin-Host':'sapidev.ccedev.net',
-	}
-	
+        method: 'GET',
+        headers: {
+
+            'Origin-Host': hostOriginHeader,
+        }
+
 
 
     })
-    
 
-    if(response.ok){
+
+    if (response.ok) {
 
         console.log("Success!")
-        
+
         const blob = await response.blob()
         const url = URL.createObjectURL(blob)
         const link = document.createElement('a')
-        link.href=url
+        link.href = url
         document.body.appendChild(link);
         link.click()
         document.body.removeChild(link);
         URL.revokeObjectURL(url);
         return blob
 
-    }else{
+    } else {
 
-        throw({error:'Something went wrong'})
+        throw ({ error: 'Something went wrong' })
     }
 
 }
